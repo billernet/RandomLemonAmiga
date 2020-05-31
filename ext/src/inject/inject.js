8e19e00d9f78fbb73e45d715146b4bbc494d82b3
Bill Pear <billpearmain@gmail.com>
2020-05-31 17:14:14 +0100

    Some tidying and prep for submission

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -3,29 +3,45 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		var menuItem = window.frames[0].document.querySelector("img[src='images/menu/signz_links.gif']");
-		if (menuItem != null) {
-            menuItem = menuItem.closest("td");		
-
-			let newMenuItem = menuItem.cloneNode(true);
-			let newImage = newMenuItem.querySelector("img")
-			let link = newMenuItem.querySelector("a");
-
-			newImage.setAttribute("src", chrome.extension.getURL("images/signz_random.gif"));
-			newImage.style.height = "27px";
-			newImage.style.width = "49px";
-
-			link.addEventListener("click", function (event) {
-				event.preventDefault();
-				let maincontent = document.querySelector("[name='maincontent'");
-				let generatedId = Math.floor(Math.random() * 4445) + 1;
-
-				maincontent.setAttribute("src", "/games/details.php?id=" + generatedId);
-			});
-
-			menuItem.parentElement.appendChild(newMenuItem);
-		}
+		addNewButton();
 
 	}
 	}, 10);
 });
+
+document.addEventListener("keyup", function (event) {
+	if (event.altKey && event.ctrlKey && event.which == 82) {
+		goToRandomPage();
+	}
+});
+
+const addNewButton = function () {
+	var menuItem = window.frames[0].document.querySelector("img[src='images/menu/signz_links.gif']");
+	if (menuItem != null) {
+		menuItem = menuItem.closest("td");		
+
+		let newMenuItem = menuItem.cloneNode(true);
+		let newImage = newMenuItem.querySelector("img")
+		let link = newMenuItem.querySelector("a");
+
+		newImage.setAttribute("src", chrome.extension.getURL("images/signz_random.gif"));
+		newImage.style.height = "27px";
+		newImage.style.width = "54px";
+
+		link.addEventListener("click", function (event) {
+			event.preventDefault();
+			goToRandomPage();
+		});
+
+		menuItem.parentElement.appendChild(newMenuItem);
+	}
+}
+
+const goToRandomPage = function() {
+	let maincontent = document.querySelector("[name='maincontent'");
+
+	let generatedId = Math.floor(Math.random() * 4445) + 1;
+	let newUrl = "/games/details.php?id=" + generatedId;
+
+	maincontent.setAttribute("src", newUrl);
+}
