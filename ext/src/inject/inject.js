4cc76eccde19116cd21e96a813752d5c61b54a2f
Bill Pear <billpearmain@gmail.com>
2020-06-01 11:00:08 +0100

    accessiblity and selector improvements

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -3,7 +3,7 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		addNewButton();
+		addRandomButton();
 
 	}
 	}, 10);
@@ -15,18 +15,21 @@ document.addEventListener("keyup", function (event) {
 	}
 });
 
-const addNewButton = function () {
-	var menuItem = window.frames[0].document.querySelector("img[src='images/menu/signz_links.gif']");
+const addRandomButton = function () {
+	var menuItem = document.querySelector("frame[name='menu']").contentDocument.querySelector("img[src='images/menu/signz_links.gif']");
 	if (menuItem != null) {
 		menuItem = menuItem.closest("td");		
 
 		let newMenuItem = menuItem.cloneNode(true);
+		newMenuItem.setAttribute("id", "random");
 		let newImage = newMenuItem.querySelector("img")
 		let link = newMenuItem.querySelector("a");
 
 		newImage.setAttribute("src", chrome.extension.getURL("images/signz_random.gif"));
 		newImage.style.height = "27px";
 		newImage.style.width = "54px";
+		newImage.setAttribute("alt", "Random game.");
+		newImage.setAttribute("title", "Go to a random game")
 
 		link.addEventListener("click", function (event) {
 			event.preventDefault();
